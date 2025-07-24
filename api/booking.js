ed7c2d9cae17155632c97d1a880b1aa035658c9a
SugeshanNaidoo <theanimeguys13@gmail.com>
2025-07-24 12:10:05 +0200

    packages
    additions

--- api/booking.js
+++ api/booking.js
@@ -56,7 +56,8 @@ export default async function handler(req, res) {
         }
 
         // Create transporter
-        const transporter = nodemailer.createTransporter({
+        const transporter = nodemailer.createTransport({
+
             host: process.env.SMTP_HOST || 'smtp.gmail.com',
             port: process.env.SMTP_PORT || 587,
             secure: false, // true for 465, false for other ports
